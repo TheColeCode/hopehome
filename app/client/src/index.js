4720c696eb88e3cc36a2c866239935cf606f359e
Jonathan Day <jon@jonuday.com>
2019-04-14 23:04:52 -0700

    Fix react issues and wire up single component.

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Home from './Components/Home';
+import Home from './Views/Home';
 // import Children from './Components/Children';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+    
     <Router>
-        <App>
-            <Route exact path='/' component={Home}/>
-	    </App>
-	</Router>
+      <App>
+        <Route exact path='/' component={Home}/>
+	  </App>
+    </Router>
+    
 ,document.getElementById('root'));
 
 
